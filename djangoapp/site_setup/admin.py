79c701a6ba57fce44e2ce03d5141934a569de077
RuanGemmer <ruangemmer@live.com>
2023-07-27 18:50:55 -0300

    Created Favicon field in site-setup and configured automatic image resizing to 32x32px

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.http.request import HttpRequest
 from site_setup.models import MenuLink, SiteSetup
+from django import forms
 
 
 # @admin.register(MenuLink)
@@ -15,10 +16,20 @@ class MenuLinkInLine(admin.TabularInline):
     extra = 1
 
 
+class SiteSetupForm(forms.ModelForm):
+    class Meta:
+        model = SiteSetup
+        fields = '__all__'
+        widgets = {
+            'favicon': forms.FileInput(attrs={'accept': 'image/png'}),
+        }
+
+
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     list_display = 'title', 'description',
     inlines = (MenuLinkInLine,)
+    form = SiteSetupForm
 
     def has_add_permission(self, request: HttpRequest) -> bool:
         return not SiteSetup.objects.exists()
--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from utils.model_validators import validate_png
+from utils.images import resize_image
 
 
 class SiteSetup(models.Model):
@@ -14,6 +16,24 @@ class SiteSetup(models.Model):
     show_description = models.BooleanField(default=True)
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%m/',
+        blank=True,
+        default='',
+        validators=[validate_png],
+        help_text='Somente imagens "PNG". Preferencialmente 32x32px.',
+    )
+
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)
+        super().save(*args, **kwargs)
+        favicon_changed = False
+
+        if self.favicon:
+            favicon_changed = current_favicon_name != self.favicon.name
+
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
     def __str__(self):
         return self.title
@@ -32,7 +52,7 @@ class MenuLink(models.Model):
         on_delete=models.CASCADE,
         blank=True,
         null=True,
-        default=None
+        default=None,
     )
 
     def __str__(self):
