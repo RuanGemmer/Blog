7e8886a98381ab68f7278434fbbcfde9645b69aa
RuanGemmer <ruangemmer@live.com>
2023-07-27 23:17:46 -0300

    configurated HTML and CSS

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, page, post
 
 
 app_name = 'blog'
 
 urlpatterns = [
+    path('post/', post, name='post'),
+    path('page/', page, name='page'),
     path('', index, name='index'),
 ]
